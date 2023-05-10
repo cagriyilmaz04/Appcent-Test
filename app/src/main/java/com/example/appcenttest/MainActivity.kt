0e4daf4553b7dc72e3b651a8b90c11ad8d70f5bc
cagriyilmaz04 <yilmaz004@outlook.com>
2023-05-11 01:18:40 +0300

    Implement Libraries and Navigation

---
+++ app/src/main/java/com/example/appcenttest/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.appcenttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.appcenttest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var navController= Navigation.findNavController(this,R.id.navHostFragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+
+
+}
